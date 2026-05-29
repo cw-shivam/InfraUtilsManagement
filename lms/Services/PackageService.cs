7258891887eaee0a3d2c2700bbcdd10416958624
Shivam Chouhan <chouhan.shivam@carwale.com>
2026-05-29 15:44:59 +0530

    Revert "fix errors"
    This reverts commit ab33448da655683a1a112d50b57cb9092454fb75.

--- lms/Services/PackageService.cs
+++ lms/Services/PackageService.cs
@@ -95,6 +95,11 @@ namespace lms.Services
                     ex
                 );
             }
+            catch (PackageVersionsFetchException)
+            {
+                // Re-throw our explicit HTTP status code failure directly without wrapping it again
+                throw;
+            }
             catch (Exception ex)
             {
                 _logger.LogError(
